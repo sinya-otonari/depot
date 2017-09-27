97ddbff8fe7f19697df0d2b2ff9a3311267fc0b9
sinya otonari <sinya.otonari6@gmail.com>
2017-09-27 20:53:57 +0900

    TASK F4 Finished

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def hidden_div_if(condition, attributes = {}, &block)
+    if condition
+      attributes["style"] = "desplay: none"
+    end
+    content_tag("div", attributes, &block)
+  end
 end
