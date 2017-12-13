57215a175d3fe1b2b06b226c61469813346b156a
sinya otonari <sinya.otonari6@gmail.com>
2017-12-13 20:55:15 +0900

    TASK I2 Finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Depot::Application.routes.draw do
+  get 'admin' => "admin#index"
+
+  controller :sessions do
+    get    'login' => :new
+    post   'login' => :create
+    delete 'login' => :destroy
+  end
+
   resources :users
 
 
@@ -10,9 +18,7 @@ Depot::Application.routes.draw do
 
   resources :carts
 
-
   get "store/index"
-
   resources :products do
     get :who_bought, on: :member
   end
