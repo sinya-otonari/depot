44f9d1e7794dca460ce43c098e9402505700e1b5
sinya otonari <sinya.otonari6@gmail.com>
2018-02-07 20:34:56 +0900

    TASK J3 Finished

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -57,7 +57,8 @@ class OrdersController < ApplicationController
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
         OrderNotifier.received(@order).deliver
-        format.html { redirect_to store_url, notice: 'ご注文ありがとうございます' }
+        format.html { redirect_to store_url, notice:
+          I18n.t('.thanks') }
         format.json { render json: @order, status: :created, location: @order }
       else
         @cart = current_cart
